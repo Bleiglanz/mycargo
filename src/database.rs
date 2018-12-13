bb894d2012e7f0d44c2be882616cc641b8cc20c2
Anton Rechenauer <antonrechenauer@gmail.com>
2018-12-13 13:53:53 +0100

    all sheets, no escape

--- src/database.rs
+++ src/database.rs
@@ -1,90 +1,96 @@
-//extern crate time;
-//extern crate chrono;
-extern crate postgres_array;
-extern crate postgres;
+// //extern crate time;
+// //extern crate chrono;
+// #[allow(dead_code)]
+// extern crate postgres_array;
+// extern crate postgres;
 
-use self::postgres_array::Array;
-use self::postgres::{Connection, TlsMode};
-use self::postgres::types::FromSql;
-use self::postgres::Result as PgResult;
-//use self::chrono::prelude::*;
-//use self::time::Timespec;
+// use self::postgres_array::Array;
+// use self::postgres::{Connection, TlsMode};
+// use self::postgres::types::FromSql;
+// use self::postgres::Result as PgResult;
+// //use self::chrono::prelude::*;
+// //use self::time::Timespec;
 
-fn get_single_value<T>(conn: &Connection, query: &str) -> PgResult<T> where T: FromSql
-{
-    println!("Executing query: {}", query);
-    let stmt = conn.prepare(query)?;
-    let rows = stmt.query(&[])?;
-    let row = rows.iter().next().unwrap();
-    row.get_opt(0).unwrap()
-}
+// fn get_single_value<T>(conn: &Connection, query: &str) -> PgResult<T> where T: FromSql
+// {
+//     println!("Executing query: {}", query);
+//     let stmt = conn.prepare(query)?;
+//     let rows = stmt.query(&[])?;
+//     let row = rows.iter().next().unwrap();
+//     row.get_opt(0).unwrap()
+// }
 
-pub fn execute() {
-    let dsn = "postgresql://antonecto:onlylocal@localhost/antondb";
-    let conn = match Connection::connect(dsn, TlsMode::None) {
-        Ok(conn) => conn,
-        Err(e) => {
-            println!("Connection error: {:?}", e);
-            return;
-        }
-    };
+// fn connect() -> Connection {
+//     let dsn = "postgresql://antonecto:onlylocal@localhost/antondb";
+//     match Connection::connect(dsn, TlsMode::None){
+//         Ok(conn) => conn,
+//         Err(e) => {
+//             println!("Connection error: {:?}", e);
+//             panic!("no connection");
+//         }
+//     }
+// }
+// #[allow(unused_variables)]
+// pub fn execute() {
 
-    let contents = std::fs::read_to_string("sql/create.sql").expect("Something went wrong reading the sql");
-    conn.batch_execute(&contents).expect("sql creation failed");
+//     let conn = connect();
 
-    conn.execute("create table if not exists blog (
-        id serial primary key,
-        title \
-                  varchar(255),
-        body text)",
-                 &[])
-        .expect("Table creation failed");
-    let stmt = match conn.prepare("insert into blog (title, body) values ($1, $2)") {
-        Ok(stmt) => stmt,
-        Err(e) => {
-            println!("Preparing query failed: {:?}", e);
-            return;
-        }
-    };
-    for i in 1..5 {
-        let title = format!("Blogpost number {}", i);
-        let text = format!("Content of the blogpost #{}", i);
-        stmt.execute(&[&title, &text]).expect("Inserting blogposts failed");
-    }
-    let stmt = match conn.prepare("select id, title, body from blog where id < $1") {
-        Ok(stmt) => stmt,
-        Err(e) => {
-            println!("Preparing query failed: {:?}", e);
-            return;
-        }
-    };
-    let max_id: i32 = 3;
-    let rows = stmt.query(&[&max_id]).expect("Selecting blogposts failed");
-    for row in rows.iter() {
-        let id: i32 = row.get("id");
-        let title: String = row.get("title");
-        println!("ID={}, title={}", id, title);
-    }
-    println!("{:?}", get_single_value::<bool>(&conn, "select 1=1"));
-    println!("{:?}", get_single_value::<i32>(&conn, "select 1=1"));
+//     let contents = std::fs::read_to_string("sql/create.sql").expect("Something went wrong reading the sql");
+//     conn.batch_execute(&contents).expect("sql creation failed");
 
-    type IntArray = Array<Option<i32>>;
-    let arr = get_single_value::<IntArray>(&conn, "select '{4, 5, 6}'::int[]");
-    println!("{:?}",
-             arr.map(|arr| {
-                 arr.iter()
-                     .filter_map(|x| *x)
-                     .collect::<Vec<_>>()
-             }));
+//     conn.execute("create table if not exists blog (
+//         id serial primary key,
+//         title \
+//                   varchar(255),
+//         body text)",
+//                  &[])
+//         .expect("Table creation failed");
+//     let stmt = match conn.prepare("insert into blog (title, body) values ($1, $2)") {
+//         Ok(stmt) => stmt,
+//         Err(e) => {
+//             println!("Preparing query failed: {:?}", e);
+//             return;
+//         }
+//     };
+//     for i in 1..5 {
+//         let title = format!("Blogpost number {}", i);
+//         let text = format!("Content of the blogpost #{}", i);
+//         stmt.execute(&[&title, &text]).expect("Inserting blogposts failed");
+//     }
+//     let stmt = match conn.prepare("select id, title, body from blog where id < $1") {
+//         Ok(stmt) => stmt,
+//         Err(e) => {
+//             println!("Preparing query failed: {:?}", e);
+//             return;
+//         }
+//     };
+//     let max_id: i32 = 3;
+//     let rows = stmt.query(&[&max_id]).expect("Selecting blogposts failed");
+//     for row in rows.iter() {
+//         let id: i32 = row.get("id");
+//         let title: String = row.get("title");
+//         println!("ID={}, title={}", id, title);
+//     }
+//     println!("{:?}", get_single_value::<bool>(&conn, "select 1=1"));
+//     println!("{:?}", get_single_value::<i32>(&conn, "select 1=1"));
 
-    //let json = get_single_value::<Json>(&conn, "select '{\"foo\": \"bar\", \"answer\": 42}'::json");
-    //println!("{:?}", json);
+//     type IntArray = Array<Option<i32>>;
+//     let arr = get_single_value::<IntArray>(&conn, "select '{4, 5, 6}'::int[]");
+//     // println!("{:?}",
+//     //          arr.map(|arr| {
+//     //              arr.iter()
+//     //                  .filter_map(|x| *x)
+//     //                  .collect::<Vec<_>>()
+//     //          }));
 
-    // let range = get_single_value::<Range<i32>>(&conn, "select '[10, 20)'::int4range");
-    // println!("{:?}", range);
-    // let ts_range =
-    // get_single_value::<Range<Timespec>>(&conn, "select '[2015-01-01, 2015-12-31]'::tsrange");
-    // println!("{:?}", ts_range);
+//     //let json = get_single_value::<Json>(&conn, "select '{\"foo\": \"bar\", \"answer\": 42}'::json");
+//     //println!("{:?}", json);
 
-    // sql_macro();
-}
+//     // let range = get_single_value::<Range<i32>>(&conn, "select '[10, 20)'::int4range");
+//     // println!("{:?}", range);
+//     // let ts_range =
+//     // get_single_value::<Range<Timespec>>(&conn, "select '[2015-01-01, 2015-12-31]'::tsrange");
+//     // println!("{:?}", ts_range);
+
+//     // sql_macro();
+// }
--- src/excel.rs
+++ src/excel.rs
@@ -3,76 +3,66 @@ extern crate calamine;
 
 use self::calamine::{open_workbook, Xlsx, Reader, DataType, Range};
 use self::postgres::{Connection,TlsMode};
-//use self::postgres::types::FromSql;
-//use self::postgres::Result as PgResult;
 
-pub fn get_range(path:&str, sheet:&str) -> Option<Range<DataType>> {
+
+fn get_ranges(path:&str) -> Vec<(String,Range<DataType>)> {
     let mut workbook: Xlsx<_> = open_workbook(path).expect("Cannot open file");
-    if let Some(Ok(range)) = workbook.worksheet_range(sheet) {
-        Some(range)
-    }else{
-        None
+    let sheets = workbook.sheet_names().to_owned();
+    let mut result:Vec<(String,Range<DataType>)> = Vec::new();
+    for s in sheets {
+        if let Some(Ok(range)) = workbook.worksheet_range(&s) {
+             result.push((String::from(s),range));
+        }
     }
+    result
 }
 
 pub fn readfile(){
 
-    const PATH:&str = "/home/anton/Temp/testdaten/tameld.xlsx";
-    const SHEETNAME:&str = "RawData";
+    //
+    // CREATE THE DATABASE, DONE ONLY ONCE
+    //
+
+    print!("Create Table in Database");
     const SQLFILE:&str = "sql/create.sql";
-    let pval = format!("{}",PATH);
-    let sval = format!("{}",SHEETNAME);
-
-    println!("Create Table in Database");
     let dsn = "postgresql://antonecto:onlylocal@localhost/antondb";
-    let conn = match Connection::connect(dsn, TlsMode::None) {
-        Ok(conn) => conn,
-        Err(e) => { println!("Connection error: {:?}", e); return; }
-    };
-
-    let create = std::fs::read_to_string(SQLFILE).expect("Err reading the sql");
+    let conn:Connection = Connection::connect(dsn, TlsMode::None).unwrap();
+    let create = std::fs::read_to_string(SQLFILE).expect("Error reading the sql");
     conn.batch_execute(&create).expect("sql creation failed");
+    println!("...done");
 
-    println!("Excel Reader");
+    upload_file("/home/anton/Temp/testdaten/tameld.xlsx",&conn);
+    upload_file("/home/anton/Temp/testdaten/Goetzfried_01_ArbeitsschritteEquipments.xlsx",&conn);
+}
 
-    // Read whole worksheet data and provide some statistics
-    if let Some(range) = get_range(&PATH,&SHEETNAME){
+
+pub fn upload_file(path:&str,conn:&Connection){
+
+    for (sheetname, range) in get_ranges(path){
         let (rows,cols) = range.get_size();
         let total_cells = rows * cols;
         let non_empty_cells: usize = range.used_cells().count();
-        println!("Zellen: {}",total_cells);
-        assert_eq!(non_empty_cells, range.rows()
-                   .flat_map(|r| r.iter().filter(|&c| c != &DataType::Empty)).count());
-
+        assert_eq!(non_empty_cells, range.rows().flat_map(|r| r.iter().filter(|&c| c != &DataType::Empty)).count());
+        if non_empty_cells == 0 { return; };
         let mut insert = "insert into allexcel (filename, sheetname, rownumber ".to_string();
         for c in 0..cols {
             insert.push_str(&format!(",s{:03} ",c));
         }
-        insert.push_str(") values (unnest($1::text[]), unnest($2::text[]), unnest($3::bigint[])");
-        for c in 0..cols {
-            insert.push_str(&format!(", unnest(${}::text[])",c+4));
+        insert.push_str(") values ");
+        for (row_idx,row) in range.rows().enumerate() {
+            insert.push_str(if row_idx==0 {""} else {","} );
+            insert.push_str(&format!("('{}','{}','{}'",path,sheetname,row_idx));
+            for (col_idx,col) in row.iter().enumerate() {
+                let cell = format!(",'{}'",col).to_string();
+                insert.push_str(&cell.trim());
+            }
+            insert.push_str(")")
         }
-        insert.push_str(")");
-        println!("{}",insert);
         let stmt = match conn.prepare(&insert) {
             Ok(stmt) => stmt,
-            Err(e) => { println!("Preparing query failed: {:?}", e); return; }
+            Err(e) => { println!("Preparing query failed: {:?}", e); println!("{}",insert); return; }
         };
-        println!("Statement bereit, lies Daten"); //Todo: unten
-
-        let val_filename  = vec![&PATH;rows];
-        let val_sheetname = vec![&SHEETNAME;rows];
-        let mut val_rownumber = vec![0;rows];
-        let mut val_sxxx = vec![vec!["";rows];cols];
-        for (row_idx,row) in range.rows().enumerate() {
-            val_rownumber[row_idx]=row_idx;
-            for (col_idx,col) in row.iter().enumerate() {
-                
-                val_sxxx[col_idx][row_idx]=&(format!("{}",col).trim().to_string());
-
-                //                println!("Zeile {}",row_idx);
-//                stmt.execute(&[&pval, &sval, &(row_idx as i64), &(_col_idx as i64), &value]).expect("db insert failed!");
-            }
-        }
+        stmt.execute(&[]).expect("Inserting sheet failed");
+        println!("Blatt {} eingefügt ",sheetname);
     }
 }
